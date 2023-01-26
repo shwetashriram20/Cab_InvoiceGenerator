d9641bf53b62235eee3e7812f40697685d175b26
shwetashriram20 <120362407+shwetashriram20@users.noreply.github.com>
2023-01-26 21:23:09 +0530

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -6,10 +6,7 @@
         {
             Console.WriteLine("***** Welcome To Cab Invoice Generator Program. *****");
 
-            InvoiceGenerator invoiceGenerator = new InvoiceGenerator(RideType.NORMAL);
-
-            double fare = invoiceGenerator.CalculateFare(2.0, 6);
-            Console.WriteLine(fare);
+           
         }
     }
 }
